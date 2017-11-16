7283a661dbd17281e30e12798089ac886328056b
Joel Åman <Schoelxz@gmail.com>
2017-11-16 15:39:25 +0100

    Player now works a bit better.
    Added a static int playcount (amount of player classes created)

--- GameClient/src/gameobjects/Player.cpp
+++ GameClient/src/gameobjects/Player.cpp
@@ -3,6 +3,8 @@
 
 #include <app/SharedContext.h>
 
+static int playerCount = 0;
+
 Player::Player() : m_MousePressed(false)
 {
 	m_Sprite.setSize(sf::Vector2f(40, 80));
@@ -10,12 +12,23 @@ Player::Player() : m_MousePressed(false)
 	m_Speed = 300;
 }
 
-Player::Player(SharedContext * context) : GameObject::GameObject(context)
+Player::Player(SharedContext * context, bool owned) : GameObject::GameObject(context), m_ClientOwned(owned)
 {
+	
 	m_Sprite.setSize(sf::Vector2f(40, 80));
 	m_Sprite.setOrigin(m_Sprite.getSize().x / 2, m_Sprite.getSize().y / 4);
+	if(playerCount > 0)
 	m_Sprite.setFillColor(sf::Color::Blue);
+	else
+	m_Sprite.setFillColor(sf::Color::Red);
 	m_Speed = 300;
+	m_PlayerInfo.IP = sf::IpAddress::getLocalAddress();
+	m_PlayerInfo.Speed = m_Speed;
+	m_ClientOwned = owned;
+	m_PlayerInfo.ID = 0;
+
+
+	playerCount++;
 }
 
 Player::~Player()
@@ -24,39 +37,50 @@ Player::~Player()
 
 void Player::Update(const float& dt)
 {
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
-	{
-		m_Sprite.move(sf::Vector2f(-m_Speed * dt, 0));
-	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
-	{
-		m_Sprite.move(sf::Vector2f(m_Speed * dt, 0));
-	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
-	{
-		m_Sprite.move(sf::Vector2f(0, -m_Speed * dt));
-	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
-	{
-		m_Sprite.move(sf::Vector2f(0, m_Speed * dt));
-	}
-
-	if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && !m_MousePressed)
-	{
-		Shoot();
-	}
-	else if (!sf::Mouse::isButtonPressed(sf::Mouse::Left) && m_MousePressed)
-	{
-		m_MousePressed = false;
-	}
-
 	// Update the players info
 	m_PlayerInfo.Position = m_Sprite.getPosition();
+	if (!m_ClientOwned) return;
+
+	if (m_Context->window->hasFocus())
+	{
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+		{
+			m_Sprite.move(sf::Vector2f(-m_Speed * dt, 0));
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+		{
+			m_Sprite.move(sf::Vector2f(m_Speed * dt, 0));
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+		{
+			m_Sprite.move(sf::Vector2f(0, -m_Speed * dt));
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+		{
+			m_Sprite.move(sf::Vector2f(0, m_Speed * dt));
+		}
+
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && !m_MousePressed)
+		{
+			Shoot();
+		}
+		else if (!sf::Mouse::isButtonPressed(sf::Mouse::Left) && m_MousePressed)
+		{
+			m_MousePressed = false;
+		}
+	}
+
 }
 
 const PlayerInfo& Player::GetPlayerInfo() const
 {
 	return m_PlayerInfo;
+}
+
+void Player::SetPlayerInfo(PlayerInfo info)
+{
+	m_PlayerInfo = info;
+	m_Sprite.setPosition(m_PlayerInfo.Position);
 }
 
 void Player::Draw(sf::RenderWindow& window)
--- GameClient/src/gameobjects/Player.h
+++ GameClient/src/gameobjects/Player.h
@@ -10,21 +10,25 @@ class Player : public GameObject
 	using GameObject::GameObject;
 public:
 	Player();
-	Player(SharedContext* context);
+	Player(SharedContext* context, bool owned);
 	~Player();
 
 	virtual void				Draw(sf::RenderWindow& window) override;
 	virtual void				Update(const float& dt) override;
 
 	const PlayerInfo&			GetPlayerInfo() const;
+	void						SetPlayerInfo(PlayerInfo info);
 
 private:
+	//static int					playerCount;
+
 	void						Shoot();
 
 	PlayerInfo					m_PlayerInfo;
 	sf::RectangleShape			m_Sprite;
 	unsigned short				m_Speed;
 	bool						m_MousePressed;
+	bool						m_ClientOwned;
 
 	std::vector<GameObject*>	m_Projectiles;
 };
